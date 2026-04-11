69432ed59c7527f5c5ba39c50281dab2f5a2bdfa
Chris O <cohara@Chriss-MacBook-Air.local>
2026-04-11 10:01:57 -0400

    Updates

--- music/iteration-2/files.inline.js
+++ music/iteration-2/files.inline.js
@@ -5,7 +5,6 @@ globalThis.__ITERATION_2_MANIFEST__ = {
   "04-smoke-filled-rooms": "04 Smoke-Filled Rooms.mp3",
   "05-passing-thoughts": "05 Passing Thoughts.mp3",
   "06-reorient": "06 Reorient.mp3",
-  "07-recursion": "07 Recursion.mp3",
   "08-the-weighout": "08 The Weigh Out.mp3",
   "09-permaculture": "09 (permaculture).mp3"
 };
